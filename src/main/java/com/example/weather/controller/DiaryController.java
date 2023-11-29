6c395d24b765be80d6895363b29e19375594788f
SuhSaechan <chan4760@gmail.com>
2023-11-29 22:21:46 +0900

    DiaryService CRUD 기능 추가

--- src/main/java/com/example/weather/controller/DiaryController.java
+++ src/main/java/com/example/weather/controller/DiaryController.java
@@ -1,7 +1,61 @@
 package com.example.weather.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.weather.domain.Diary;
+import com.example.weather.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class DiaryController {
+    private final DiaryService diaryService;
+    @PostMapping("/create/diary")
+    void createDiary(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestBody String text
+    ) {
+        diaryService.createDiary(date,text);
+    }
+
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date
+    ){
+        return diaryService.readDiary(date);
+    }
+
+    @GetMapping("/read/diaries")
+    List<Diary> readDiaries(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate endDate
+    ){
+        return diaryService.readDiaries(startDate,endDate);
+    }
+
+    @PutMapping("/update/diary")
+    void updateDiary(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date,
+            @RequestBody String text
+    ){
+        diaryService.updateDiary(date, text);
+    }
+
+    @DeleteMapping("/delete/diary")
+    void deleteDiary(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date
+    ){
+        diaryService.deleteDiary(date);
+    }
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,12 @@ create table memo (
     Id INT not null primary key auto_increment,
     text varchar(50) not null
 );
+
+create table diary(
+    id INT not null primary key auto_increment,
+    weather VARCHAR(50) NOT NULL,
+    icon VARCHAR(50) NOT NULL ,
+    temperature DOUBLE NOT NULL,
+    text VARCHAR(500) NOT NULL,
+    date DATE NOT NULL
+)
